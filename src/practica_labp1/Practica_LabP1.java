f228083d3651874d187accbe260a444c1dcecdc1
multr <multr@genshin>
2022-04-22 15:15:46 -0600

    El anexo Clase Alumno

---
+++ src/practica_labp1/Practica_LabP1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_labp1;
+
+/**
+ *
+ * @author multr
+ */
+public class Practica_LabP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Especial Characters 
+        \n = Line Jump
+        */
+        System.out.println("Hello World!");
+        System.out.println("- Valor Numerico "+(12.34+15.25)+"$ \nEstado: "+true+"- habilitado");
+        System.out.print("Datos de alumnos\nCuenta: 22121005\nNombre Alumno: Scott Pang\nCarrera: Ingenieria Sistemas\n");
+        
+      
+    }
+    
+}
